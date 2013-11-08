8a21a34da1d7c3b358adcb3809014a9171652eb0
Vaibhav Sharma <vaibhavign@gmail.com>
2013-11-08 13:26:24 +0530

    proper formatted and logged functionality completed

--- update-cod-pin-codes.php
+++ update-cod-pin-codes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Eshopbox update cod pincodes
 Plugin URI: http://www.vaibhavign.com
-Description: Bluedart integration panel
+Description: update cod pincodes panel for aramex and bluedart
 Version: 1.0
 Author: Vaibhav Sharma
 Author Email: http://www.vaibhavign.com
@@ -33,14 +33,14 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class WC_updatepincodes{
     public function __construct(){
-        add_action('admin_menu', array( &$this, 'woocommerce_bluedart_admin_menu' )); 
+        add_action('admin_menu', array( &$this, 'woocommerce_updatepincode_admin_menu' )); 
        
     }
                             
-function woocommerce_bluedart_admin_menu() {
+function woocommerce_updatepincode_admin_menu() {
     
-    add_menu_page(__('Update pincode','wc-bluedart'), __('Update pincode','wc-bluedart'), 'edit_posts', 'eshopbox-pincode', array( &$this, 'update_pincode_page' ) );
-    add_submenu_page( 'eshopbox-bluedart', 'Config', 'Config', 'edit_posts', 'bluedart_config', array( &$this, 'bluedart_config_page' ) );
+    add_menu_page(__('Update pincode','wc-pincode'), __('Update pincode','wc-pincode'), 'edit_posts', 'eshopbox-pincode', array( &$this, 'update_pincode_page' ) );
+ 
 }
  
 function update_pincode_page (){
@@ -98,196 +98,141 @@ function update_pincode_page (){
 		  </div>
 		</div>
 <?php
-if($_POST['post1']=='post'){
-  
-       //  echo '<pre>'; print_r($_FILES);
-       //  print_r($_POST); exit;
-   if(!isset($_POST['rad'])){die('Please select an option');}      
-	// for text files
-// for excel file
-//	if($_FILES['csvtext']['type']=='application/vnd.ms-excel'){
-  $myFile = $_FILES['batch']['tmp_name']; 
-//set_include_path(get_include_path() . PATH_SEPARATOR . 'class/');
-include_once('class/PHPExcel/IOFactory.php');
-try {
-    
-$objReader = PHPExcel_IOFactory::createReader('Excel5');
+    if($_POST['post1']=='post'){
+        if(!isset($_POST['rad'])){die('Please select an option');}      
+        $myFile = $_FILES['batch']['tmp_name']; 
+        include_once('class/PHPExcel/IOFactory.php');
+        try {
+            $objReader = PHPExcel_IOFactory::createReader('Excel5');
+            $objPHPExcel = $objReader->load($myFile);
+        } catch(Exception $e) {
+            die('Error loading file "'.pathinfo($myFile,PATHINFO_BASENAME).'": '.$e->getMessage());
+        }
+    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+    $i=0;
+    if($_POST['rad']=='aramex'){
+    // if sheet is of aramex
+        foreach($sheetData as $sheetdata){
+            $pincodes = trim($sheetdata['A']);
+            $cityname =  trim($sheetdata['C']); 
+            $statename =  trim($sheetdata['E']);
+            if(trim($sheetdata['M'])=='Y'){
+                $pincode[$pincodes]= $pincodes ;
+                $pincodea[$pincodes]['city']= $cityname ;
+                $pincodea[$pincodes]['state']= $pincodes ; 
+                $pincodea[$pincodes]['cod']= 'Y' ;
+                $i++;
+            }
+        }
+        $querystr = "SELECT pincode FROM ".$wpdb->prefix."cod_aramex";
+        $postid = $wpdb->get_results($querystr,ARRAY_A);
+        foreach($postid as $key=>$val){
+            $dbpincode[$val['pincode']]=$val['pincode'];
+        }
+        $arrayToINsert = array_diff($pincode,$dbpincode);
+        $arrayToDelete = array_diff($dbpincode,$pincode);
+        //echo sizeof($arrayToINsert).'--'.sizeof($arrayToDelete);
+        $ax = 0;
+        foreach($arrayToDelete as $key=>$val){
+            $querystr = $wpdb->get_results("delete FROM ".$wpdb->prefix."cod_aramex where pincode=".$val);
+            $ax++; 
+        }
 
-$objPHPExcel = $objReader->load($myFile);
+        echo $ax." rows deleted </br>";
+        $bx = 0;
+        foreach($arrayToINsert as $key=>$val){
+            $wpdb->insert( 
+               $wpdb->prefix.'cod_aramex', 
+                array( 
+                   'pincode' => $val,
+                   'city' => $pincodea[$val]['city'],
+                   'state'=>$pincodea[$val]['state'],
+                   'cod'=> $pincodea[$val]['cod']
+                    )
+                );
+            $bx++;
+        }
+        echo $bx." rows added </br>";
 
-//	$objPHPExcel = PHPExcel_IOFactory::load($myFile);
+        // add to log the operation done
+        $filename = 'process.txt';
+        $file = WP_PLUGIN_DIR."/update-cod-pin-codes/".$filename; 
+        $message = " \n Log created dated ".date('d-m-Y h-m-s')." \n";
+        $message .= $ax." rows deleted from aramex with pincodes ".implode(',',$arrayToDelete)." \n";
+        $message .= $bx." rows added to aramex with pincodes ".implode(',',$arrayToINsert)." \n";
+        $message .= "***************************************************************** \n";
+        // Write the contents to the file, 
+        // using the FILE_APPEND flag to append the content to the end of the file
+        // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+        file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
+    exit;
 
-//echo 'test123'; exit;
-} catch(Exception $e) {
 
-	die('Error loading file "'.pathinfo($myFile,PATHINFO_BASENAME).'": '.$e->getMessage());
+    } else if($_POST['rad']=='bluedart'){
+        // if sheet is of bluedart
+        foreach($sheetData as $sheetdata){
+            $pincodes = trim($sheetdata['C']);  //bluedart
+            if(trim($sheetdata['N'])=='Yes'){  //bluedart
+                //$pincodes = trim($sheetdata['A']);
+                $cityname =  trim($sheetdata['D']); 
+                $statename =  trim($sheetdata['F']);
+                //if(trim($sheetdata['M'])=='Y'){
+                $pincode[$pincodes]= $pincodes ;
+                $pincodea[$pincodes]['city']= $cityname ;
+                $pincodea[$pincodes]['state']= $statename ;
+                $pincodea[$pincodes]['cod']= 'Yes' ;
+                //$pincode[$i] = $pincodes ;
+                //print_r($sheetdata);
+                $i++;
+            }
+        }
 
+        $querystr = "SELECT pincode FROM ".$wpdb->prefix."bluedart_codpins";
+        $postid = $wpdb->get_results($querystr,ARRAY_A);
+        foreach($postid as $key=>$val){
+            $dbpincode[$val['pincode']]=$val['pincode'];
+        }
+
+        $arrayToINsert = array_diff($pincode,$dbpincode);
+        $arrayToDelete = array_diff($dbpincode,$pincode);
+        $ax = 0;
+        foreach($arrayToDelete as $key=>$val){
+            $querystr = $wpdb->get_results("delete FROM ".$wpdb->prefix."bluedart_codpins where pincode=".$val);
+            $ax++; 
+        }
+        echo $ax." rows deleted </br>";
+        $bx = 0;
+        foreach($arrayToINsert as $key=>$val){
+             $wpdb->insert( 
+                $wpdb->prefix.'bluedart_codpins', 
+                    array( 
+                    'pincode' => $val,
+                    'city' => $pincodea[$val]['city'],
+                    'state'=>$pincodea[$val]['state'],
+                    'cod'=> $pincodea[$val]['cod']
+                    )
+                );
+         $bx++;
+    }
+
+        echo $bx." rows added </br>";
+
+        $filename = 'process.txt';
+        $file = WP_PLUGIN_DIR."/update-cod-pin-codes/".$filename; 
+        $message = " \n Log created dated ".date('d-m-Y h-m-s')." \n";
+        $message .= $ax." rows deleted from bluedart with pincodes ".implode(',',$arrayToDelete)." \n";
+        $message .= $bx." rows added to bluedart with pincodes ".implode(',',$arrayToINsert)." \n";
+        $message .= "*****************************************************************  \n";
+        // Write the contents to the file, 
+        // using the FILE_APPEND flag to append the content to the end of the file
+        // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+        file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
+        exit;
+    }
+  }
 }
 
-
-
-$sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-
-//echo '<pre>';
-//print_r($sheetData);
-//exit;
-$i=0;
-if($_POST['rad']=='aramex'){
-// if sheet is of aramex
-foreach($sheetData as $sheetdata){
-	//$pincodes = trim($sheetdata['C']);  bluedart
-//if(trim($sheetdata['N'])=='Yes'){  bluedart
-	$pincodes = trim($sheetdata['A']);
-        $cityname =  trim($sheetdata['C']); 
-        $statename =  trim($sheetdata['E']);
-if(trim($sheetdata['M'])=='Y'){
-$pincode[$pincodes]= $pincodes ;
-$pincodea[$pincodes]['city']= $cityname ;
-$pincodea[$pincodes]['state']= $pincodes ; 
-$pincodea[$pincodes]['cod']= 'Y' ;
-//$pincode[$i] = $pincodes ;
-//print_r($sheetdata);
-$i++;
-}
-}
-
-$querystr = "SELECT pincode FROM ".$wpdb->prefix."cod_aramex";
-$postid = $wpdb->get_results($querystr,ARRAY_A);
-foreach($postid as $key=>$val){
-    $dbpincode[$val['pincode']]=$val['pincode'];
-}
-
-$arrayToINsert = array_diff($pincode,$dbpincode);
-$arrayToDelete = array_diff($dbpincode,$pincode);
-
-//echo sizeof($arrayToINsert).'--'.sizeof($arrayToDelete);
-$ax = 0;
-foreach($arrayToDelete as $key=>$val){
-  //  echo "delete FROM wp_cod_dtdc where pincode=".$val."<br>";
-    $querystr = $wpdb->get_results("delete FROM ".$wpdb->prefix."cod_aramex where pincode=".$val);
-   $ax++; 
-}
-
-echo $ax." rows deleted </br>";
-
-$bx = 0;
-foreach($arrayToINsert as $key=>$val){
-   //   echo "Insert FROM wp_cod_dtdc where pincode=".$val."<br>";
-  //  $querystr = $wpdb->get_results("delete FROM wp_cod_dtdc where pincode=".$val);
-
-    $wpdb->insert( 
-	$wpdb->prefix.'cod_aramex', 
-	array( 
-		'pincode' => $val,
-                'city' => $pincodea[$val]['city'],
-                'state'=>$pincodea[$val]['state'],
-                'cod'=> $pincodea[$val]['cod']
-
-	)
-);
-   $bx++;
-}
-
-echo $bx." rows added </br>";
-
-
-
-
-//echo $i.'---';
-//echo sizeof($pincode);
-//echo 'test';
-//echo '<pre>';
-//print_r($pincode);
-//print_r($arrayToDelete);
-exit;
-} else if($_POST['rad']=='bluedart'){
-    
-    // if sheet is of bluedart
-foreach($sheetData as $sheetdata){
-	$pincodes = trim($sheetdata['C']);  //bluedart
-if(trim($sheetdata['N'])=='Yes'){  //bluedart
-	//$pincodes = trim($sheetdata['A']);
-        $cityname =  trim($sheetdata['D']); 
-        $statename =  trim($sheetdata['F']);
-//if(trim($sheetdata['M'])=='Y'){
-$pincode[$pincodes]= $pincodes ;
-$pincodea[$pincodes]['city']= $cityname ;
-$pincodea[$pincodes]['state']= $statename ;
-$pincodea[$pincodes]['cod']= 'Yes' ;
-//$pincode[$i] = $pincodes ;
-//print_r($sheetdata);
-$i++;
-}
-}
-
-$querystr = "SELECT pincode FROM ".$wpdb->prefix."bluedart_codpins";
-$postid = $wpdb->get_results($querystr,ARRAY_A);
-foreach($postid as $key=>$val){
-    $dbpincode[$val['pincode']]=$val['pincode'];
-}
-
-$arrayToINsert = array_diff($pincode,$dbpincode);
-$arrayToDelete = array_diff($dbpincode,$pincode);
-
-//echo sizeof($arrayToINsert).'--'.sizeof($arrayToDelete);
-$ax = 0;
-foreach($arrayToDelete as $key=>$val){
-  //  echo "delete FROM wp_cod_dtdc where pincode=".$val."<br>";
-    $querystr = $wpdb->get_results("delete FROM ".$wpdb->prefix."bluedart_codpins where pincode=".$val);
-   $ax++; 
-}
-
-echo $ax." rows deleted </br>";
-
-$bx = 0;
-foreach($arrayToINsert as $key=>$val){
-   //   echo "Insert FROM wp_cod_dtdc where pincode=".$val."<br>";
-  //  $querystr = $wpdb->get_results("delete FROM wp_cod_dtdc where pincode=".$val);
-
-    $wpdb->insert( 
-	$wpdb->prefix.'bluedart_codpins', 
-	array( 
-		'pincode' => $val,
-                'city' => $pincodea[$val]['city'],
-                'state'=>$pincodea[$val]['state'],
-                'cod'=> $pincodea[$val]['cod']
-
-	)
-);
-   $bx++;
-}
-
-echo $bx." rows added </br>";
-$filename = 'process.txt';
-$file = WP_PLUGIN_DIR."/update-cod-pin-codes/".$filename; 
-// The new person to add to the file
-$message = "/n Log created dated ".date('d-m-Y h-m-s')."/n";
-$message .= $ax." rows deleted from bluedart with pincodes ".implode(',',$arrayToDelete)."/n";
-$message .= $bx." rows added to bluedart with pincodes ".implode(',',$arrayToINsert)."/n";
-$message .= "***************************************************************** /n";
-// Write the contents to the file, 
-// using the FILE_APPEND flag to append the content to the end of the file
-// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
-//echo $i.'---';
-//echo sizeof($pincode);
-//echo 'test';
-//echo '<pre>';
-//print_r($pincode);
-//print_r($arrayToDelete);
-exit;
-}
-//print_r($xyz);
-
-//	}
-	//echo '<pre>';
-	//print_r($_FILES);
-         
-     }
-
-}
-
-/**
+    /**
      * Get the plugin url.
      *
      * @access public
@@ -308,7 +253,5 @@ exit;
         if ( $this->plugin_path ) return $this->plugin_path;
         return $this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
     }
-  
-
 }
 new WC_updatepincodes();
